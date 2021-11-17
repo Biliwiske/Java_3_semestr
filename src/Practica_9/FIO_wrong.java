8e8a36bf343de3e211d64aa55a6cd01cf450047e
Biliwiske <92444804+Biliwiske@users.noreply.github.com>
2021-11-17 14:51:20 +0300

    Delete src directory

--- src/Practica_9/FIO_wrong.java
+++
@@ -1,7 +0,0 @@
-package Practica_9;
-
-public class FIO_wrong extends Throwable {
-    public FIO_wrong(String data){
-        super("ФИО "+ data + " не найдена!");
-    }
-}
--- src/Practica_9/INN_wrong.java
+++
@@ -1,7 +0,0 @@
-package Practica_9;
-
-public class INN_wrong extends Throwable {
-    public INN_wrong(String data){
-        super("ИНН "+ data + " неверный!");
-    }
-}
