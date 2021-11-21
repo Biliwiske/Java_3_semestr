e90ef077601232535ebb73637d71334b716337a8
Biliwiske <92444804+Biliwiske@users.noreply.github.com>
2021-11-21 16:46:11 +0300

    Add files via upload

---
+++ Practica_11/ex2/AbstractQueue.java
@@ -0,0 +1,9 @@
+package Practica_11.ex2;
+
+import Practica_11.ex2.Queue;
+
+public abstract class AbstractQueue implements Queue {
+    protected int head, tail;
+    protected static final int START_CAPACITY = 16;
+
+}
